2aec0ab0a4c96a57c23a5787662db918c43c6618
petrprog1997 <petrprog1997@mail.ru>
2019-09-18 15:42:24 +0300

    test new methods

--- EPAM_JAVA_PROJECT_STAGE_1/src/by/epam/main/Main.java
+++ EPAM_JAVA_PROJECT_STAGE_1/src/by/epam/main/Main.java
@@ -2,6 +2,7 @@ package by.epam.main;
 
 import java.util.Arrays;
 
+import by.epam.arrayaverage.arrayAverage;
 import by.epam.arraycreate.createArray;
 import by.epam.arraysort.arraySort;
 
@@ -19,7 +20,13 @@ public class Main {
 		
 		sourseArray = sortArray.bubblesort(sourseArray);
 		
-		System.out.print(Arrays.toString(sourseArray));
+		System.out.println(Arrays.toString(sourseArray));
+		
+		arrayAverage averageArray = new arrayAverage();
+		
+		double sourseArrayAverage = averageArray.average(sourseArray);
+		
+		System.out.print(sourseArrayAverage);
 		
 	}
 	
