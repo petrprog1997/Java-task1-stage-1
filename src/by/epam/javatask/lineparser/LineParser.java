0e73d479b11bd4fa1bd835b5dfa008d260325392
petrprog1997 <55106133+petrprog1997@users.noreply.github.com>
2019-10-10 17:58:53 +0300

    Update LineParser.java

--- src/by/epam/javatask/lineparser/LineParser.java
+++ src/by/epam/javatask/lineparser/LineParser.java
@@ -3,10 +3,10 @@ package by.epam.javatask.lineparser;
 public class LineParser {
 
     public static double[] parse(String string) {
-        double[] array = new double[string.length()];
         string = string.trim().replaceAll("\\s{2,}", " ");
         String[] arrayString = string.split(" ");
-        int arrayLength = arrayString.length;
+        double[] array = new double[arrayString.length];
+        int arrayLength = array.length;
             for (int i = 0; i<arrayLength; i++){
                array[i] = Double.parseDouble(arrayString[i]);
            }
